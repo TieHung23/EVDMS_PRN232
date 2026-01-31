c59042a82c59fcf859e3e21376ecf52aef8882bf
Kimpung2901 <phungkimthi29012004@gmail.com>
2026-01-31 11:07:24 +0700

    Add validation attributes to Vehicle request DTOs
    400

--- EVDMS.BusinessLogicLayer/Dto/Request/Vehicle/VehicleCreateRequest.cs
+++ EVDMS.BusinessLogicLayer/Dto/Request/Vehicle/VehicleCreateRequest.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace EVDMS.BusinessLogicLayer.Dto.Request.Vehicle;
 
 public class VehicleCreateRequest
 {
+    [Required]
     public required string ModelName { get; set; } = string.Empty;
+    [Required]
     public required string Brand { get; set; } = string.Empty;
+    [Required]
     public required string VehicleType { get; set; } = string.Empty;
+    [Required]
     public required string Description { get; set; } = string.Empty;
+    [Range(1900, 2100)]
     public required int ReleaseYear { get; set; }
     [JsonIgnore]
     public string CreatedBy { get; set; } = string.Empty;
--- EVDMS.BusinessLogicLayer/Dto/Request/Vehicle/VehicleUpdateRequest.cs
+++ EVDMS.BusinessLogicLayer/Dto/Request/Vehicle/VehicleUpdateRequest.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace EVDMS.BusinessLogicLayer.Dto.Request.Vehicle;
 
 public class VehicleUpdateRequest
 {
+    [Required]
     public string ModelName { get; set; } = string.Empty;
+    [Required]
     public string Brand { get; set; } = string.Empty;
+    [Required]
     public string VehicleType { get; set; } = string.Empty;
+    [Required]
     public string Description { get; set; } = string.Empty;
+    [Range(1900, 2100)]
     public int ReleaseYear { get; set; }
     [JsonIgnore]
     public string ModifiedBy { get; set; } = string.Empty;
